691251981bf8facbae7a98c8cb238bc79e8bcd17
Olivier Fauchon <olivier@aixmarseille.com>
2016-08-04 07:28:23 +0200

    Minor fixes

--- oflbridge/launcher.sh
+++ oflbridge/launcher.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-./oflbridge -s /dev/ttyUSB1:115200 -i http://localhost:8086/db/metrics/series?u=user&p=password"
+USER=oflmetrics
+PASS=changeme
+
+while true; do 
+  echo "Starting oflbridge"
+  LD_LIBRARY_PATH=./xPLLib ./oflbridge -s /dev/ttyUSB1:115200 -i 'http://localhost:8086/write?db=oflmetrics&u=${USER}&p=${PASS}'
+  echo "Sleep 60" ;sleep 61
+done
